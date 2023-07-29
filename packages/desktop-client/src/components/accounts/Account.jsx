e80fd13e951e2447ddc33270986cc234fcfdd531
nfinite <nfinite@gmail.com>
2023-07-29 16:17:59 -0700

    Search filter fix

--- packages/desktop-client/src/components/accounts/Account.jsx
+++ packages/desktop-client/src/components/accounts/Account.jsx
@@ -368,9 +368,7 @@ class AccountInternal extends PureComponent {
             transactionsFiltered: isFiltered,
             loading: false,
             workingHard: false,
-            balances: this.state.showBalances
-              ? await this.calculateBalances()
-              : null,
+            balances: await this.calculateBalances(),
           },
           () => {
             if (firstLoad) {
